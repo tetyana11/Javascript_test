82e986ea7b047cbb783944cec105208e977f53f4
tetyana11 <35502019+tetyana11@users.noreply.github.com>
2018-01-28 17:27:37 +0100

    Add files via upload

---
+++ Shopping List
@@ -0,0 +1,7 @@
+/*myList should be an array
+The first elements in each of your sub-arrays must all be strings
+The second elements in each of your sub-arrays must all be numbers
+You must have at least 5 items in your list */
+
+var myList = [];
+myList = [["Chocolate", 15], ["Juice", 5], ["Bread", 3], ["Eggs", 10], ["Water", 20]];
---
+++ Write Reusable JavaScript with Functions.js
@@ -0,0 +1,17 @@
+/* reusableFunction should be a function
+reusableFunction should output "Hi World" to the dev console
+Call reusableFunction after you define it */
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ function reusableFunction() {
+  console.log("Hi World");
+}
+  reusableFunction();
+
+// Only change code below this line
+
+
