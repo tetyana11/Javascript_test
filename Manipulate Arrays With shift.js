6ea3a1636096beadea2903ec58da4b62531bf7a0
tetyana11 <35502019+tetyana11@users.noreply.github.com>
2018-01-28 14:42:58 +0100

    Add files via upload

---
+++ Manipulate Arrays With shift.js
@@ -0,0 +1,13 @@
+/*myArray should now equal [["dog", 3]].
+removedFromMyArray should contain ["John", 23].*/
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
